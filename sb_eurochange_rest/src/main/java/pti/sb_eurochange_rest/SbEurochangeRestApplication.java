e5a84195a3fcf502687d5cde1e1fc71215f30639
fadam87 <agrarmernok87@gmail.com>
2024-06-24 11:53:11 +0200

    rest_init

---
+++ sb_eurochange_rest/src/main/java/pti/sb_eurochange_rest/SbEurochangeRestApplication.java
@@ -0,0 +1,13 @@
+package pti.sb_eurochange_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEurochangeRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbEurochangeRestApplication.class, args);
+	}
+
+}
---
+++ sb_eurochange_rest/src/test/java/pti/sb_eurochange_rest/SbEurochangeRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pti.sb_eurochange_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEurochangeRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
